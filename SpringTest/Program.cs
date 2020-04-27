a9bddde0cda4389003888301ded4ce4bd9e17659
Yonatan Rubin <yooonatan@gmail.com>
2020-04-27 15:52:18 +0300

    configure to local spring server named config-client

--- SpringTest/Program.cs
+++ SpringTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 
 namespace SpringTest
 {
@@ -6,7 +9,27 @@ namespace SpringTest
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			IConfigurationBuilder builder =
+				new ConfigurationBuilder()
+					.AddJsonFile("appsettings.json")
+					.AddJsonFile("local.json")
+					.AddConfigServer("/development/master");
+
+			var cfg = builder.Build();
+			Console.WriteLine(string.Join(",", cfg.GetChildren().Select(c => c.Key)));
+			Console.WriteLine(cfg.GetDebugView());
+		}
+
+		class Config
+		{
+			public Another Another { get; set; }
+			public string Role { get; set; }
+		}
+
+		class Another
+		{
+			public string Sub { get; set; }
+			public string Key { get; set; }
 		}
 	}
 }
